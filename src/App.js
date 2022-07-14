03e5901a573df9cfa1a1c4838038a49605579b40
Moschi <smoschini@kovix-group.com>
2022-07-14 20:42:42 -0300

    desarrollo footer terminado

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Body from './component/Body';
 import { Header } from './component/Header';
+import Footer from './component/Footer';
 
 function App() {
   return (
     <>
     <Header/>
       <Body/>
+    <Footer/>
     </>
   );
 }
